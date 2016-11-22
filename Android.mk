4b47e271c350d80902b9b0da68c19f3d75f7cce0
conorf50 <conorf50@gmail.com>
2016-11-22 00:06:11 +0000

    Removed references to 'bq' and 'piccolo'
    Change-Id: I319d5351775ce6c4c85e52e0236cd7ad87934137

--- Android.mk
+++ Android.mk
@@ -16,7 +16,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter piccolo picmt vegetalte,$(TARGET_DEVICE)),)
+ifneq ($(filter p839v55 picmt vegetalte,$(TARGET_DEVICE)),)
 
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
